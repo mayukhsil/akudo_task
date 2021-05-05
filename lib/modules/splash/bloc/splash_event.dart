e260ea8e9f5655f5cb8c617a3beeaf7250f12182
mayukhsil <mayukh.sil7@gmail.com>
2021-05-06 03:28:45 +0530

    chore: updated folder structure

---
+++ lib/modules/splash/bloc/splash_event.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashEvent extends Equatable {
+  @override
+  List<Object> get props => null;
+}
+  class GetHomeDataEvent extends SplashEvent {}
